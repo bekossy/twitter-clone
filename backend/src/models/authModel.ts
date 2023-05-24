ff9881be2faa361dfbe9109662bf031f122713d9
Kaosi Ezealigo <ezealigokosiso@gmail.com>
2023-05-24 22:12:49 +0100

    adds profileName and username to auth Schema

--- backend/src/models/authModel.ts
+++ backend/src/models/authModel.ts
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const authSchema = new Schema({
-    firstName: {type:String, required: true},
-    lastName: {type: String, required: true},
+    profileName: {type: String, required: true},
     email: { type: String, unique: true, required: true },
     password: {type: String, required: true},
+    username: {type: String, required: true, unique: true},
 }, {timestamps: true})
 
 const User = mongoose.model("User", authSchema);
