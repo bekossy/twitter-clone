470fd15a3c0b8e0f1c58d479d6d811dfbae7cb36
Kaosi Ezealigo <ezealigokosiso@gmail.com>
2023-05-24 22:16:07 +0100

    sets up mongoose connect and route middlewares

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,25 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-
+import {config} from 'dotenv';
+import tweetRoutes from "./routes/tweetRoutes";
+import authRoutes from "./routes/authRoutes";
+config();
 const app = express();
 
-app.listen(5000, () => {
-    console.log("running");    
-})
+app.use(cors({
+    origin: "*"
+}))
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/tweets", tweetRoutes);
+
+mongoose.connect(process.env.MONGO_URI!)
+        .then(() => {
+            app.listen(process.env.PORT, () => {
+                console.log(`server running on port:${process.env.PORT}`)
+            })
+        })
+        .catch((err) => console.log(err))
