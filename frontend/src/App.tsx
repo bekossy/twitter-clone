df92e5d5dfc68c293111cc4b33b8043dabc14ad5
Kaosi Ezealigo <ezealigokosiso@gmail.com>
2023-05-25 09:59:19 +0100

    sets up react-router and import pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Homepage from "./pages/Homepage"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
 
 function App() {
-
-
   return (
     <>
-      <div className="app"></div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
