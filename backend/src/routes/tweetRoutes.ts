2e2c3ce2cfdbde4d10b73ed1077c00d362c4e7b5
Kaosi Ezealigo <ezealigokosiso@gmail.com>
2023-05-25 01:36:30 +0100

    adds middleware to tweet routes

--- backend/src/routes/tweetRoutes.ts
+++ backend/src/routes/tweetRoutes.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 const router = express.Router();
 import { addNewTweet, deleteTweet, editTweet, getAllTweets } from '../controllers/tweetController';
+import { authTweets } from '../middleware/authTweets';
 
+router.use(authTweets);
 router.get("/", getAllTweets);
 router.post("/", addNewTweet);
 router.delete("/:id", deleteTweet);
