713624e23e955ae4d2bfe9bc9e8c45881f026bfa
Kaosi Ezealigo <ezealigokosiso@gmail.com>
2023-05-27 14:09:05 +0100

    imports SideNav, Home and Trends component

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,10 +1,24 @@
+import { Box, styled } from "@mui/material"
+import SideNav from "../components/SideNav"
+import Home from "../components/Home"
+import Trends from "../components/Trends"
 
+const HomepageBox = styled(Box)({
+  maxWidth: "1300px",
+  width: "100%",
+  margin: "0 auto",
+  display: "flex"
+})
 
 const Homepage = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <HomepageBox>
+        <SideNav />
+        <Home />
+        <Trends />
+      </HomepageBox>
+    </>
   )
 }
 
