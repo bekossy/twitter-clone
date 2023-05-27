8a68c81c37bf2687ea9d599a8e60fb6aeed94102
Kaosi Ezealigo <ezealigokosiso@gmail.com>
2023-05-27 17:45:18 +0100

    styles component flex-grow

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import { Box } from '@mui/material';
 
 const Home = () => {
     return (
-        <div>
-
-        </div>
+        <Box flex={2}>
+            <h1>Home</h1>
+        </Box>
     )
 }
 
--- frontend/src/components/Trends.tsx
+++ frontend/src/components/Trends.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import { Box } from '@mui/material';
 
 const Trends = () => {
     return (
-        <div>
-
-        </div>
+        <Box flex={1}>
+            <Box width={"300px"} sx={{ overflow: "none" }}>
+                <h1>Trends</h1>
+            </Box>
+        </Box>
     )
 }
 
