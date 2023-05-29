e4c2c843a7ac05b3457ab1220dd41c2f89c22e6e
Kaosi Ezealigo <ezealigokosiso@gmail.com>
2023-05-29 16:16:26 +0100

    imports Icons and styles component

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,11 +1,155 @@
-import { Box } from '@mui/material';
+import { Avatar, Box, Button, Stack, TextField, Tooltip, Typography, styled } from '@mui/material';
+import ImageOutlinedIcon from '@mui/icons-material/ImageOutlined';
+import GifBoxOutlinedIcon from '@mui/icons-material/GifBoxOutlined';
+import ChecklistRoundedIcon from '@mui/icons-material/ChecklistRounded';
+import SentimentSatisfiedAltRoundedIcon from '@mui/icons-material/SentimentSatisfiedAltRounded';
+import PlaceOutlinedIcon from '@mui/icons-material/PlaceOutlined';
+import EventAvailableOutlinedIcon from '@mui/icons-material/EventAvailableOutlined';
+import { useState } from 'react';
+import Tweets from './Tweets';
+
+const HomeBox = styled(Box)({
+    flex: 2,
+    border: "1px solid rgb(239, 243, 244)",
+})
+const HomeTitle = styled(Box)({
+    position: "sticky",
+    top: 0,
+    backdropFilter: "blur(12px)",
+    backgroundColor: "rgba(255,255,255,0.7)",
+    borderBottom: "1px solid rgb(239, 243, 244)",
+    zIndex: 10
+});
+const HomeTitleOpts = styled(Box)({
+    display: "flex",
+    alignItems: "center",
+});
+const HomeTitleOpt = styled(Typography)({
+    flex: 1,
+    padding: "20px 0",
+    fontSize: "16px",
+    fontWeight: 600,
+    textAlign: "center",
+    cursor: "pointer",
+    position: "relative",
+    "&:hover": {
+        backgroundColor: "rgba(0,0,0,0.1)"
+    },
+    "&:last-of-type": {
+        color: "#536471",
+        fontWeight: 400,
+    }
+});
+const HomeContent = styled(Box)({
+    display: "flex",
+    padding: "10px 20px",
+    gap: "10px",
+    borderBottom: "1px solid rgb(239, 243, 244)",
+});
+const TweetField = styled(TextField)({
+    '& .MuiOutlinedInput-root': {
+        backgroundColor: "#fff",
+        fontSize: "20px",
+        padding: "0",
+        color: "#536471",
+        borderBottom: "1px solid rgb(239, 243, 244)",
+        '& fieldset': {
+            border: "none"
+        },
+        "& ::placeholder": {
+            color: "#000",
+            fontSize: "22px"
+        }
+    },
+
+})
+const TweetBtns = styled(Box)({
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: "5px 10px 0",
+    "& button": {
+        backgroundColor: "#1d9bf0",
+        color: "#fff",
+        padding: "5px 17px",
+        borderRadius: "25px",
+        fontSize: "16px",
+        textTransform: "capitalize",
+        fontWeight: 600,
+    },
+    "& button:hover": {
+        backgroundColor: "#0d86d6"
+    },
+    "& button:disabled": {
+        backgroundColor: "rgba(13, 134, 214, 0.4)",
+        color: "#fff"
+    },
+    "& svg": {
+        color: "#1d9bf0",
+        borderRadius: "50%",
+        fontSize: "30px",
+        padding: "5px",
+        cursor: "pointer",
+        "&:hover": {
+            backgroundColor: "rgba(13, 134, 214, 0.2)"
+        }
+    }
+});
 
 const Home = () => {
-    return (
-        <Box flex={1.5}>
-            <h1>Home</h1>
+    const [tweet, setTweet] = useState("");
 
-        </Box>
+    return (
+        <HomeBox flex={2}>
+            <HomeTitle>
+                <Typography variant='h6' component={"h1"} fontWeight={600} padding={"10px 20px"}>Home</Typography>
+                <HomeTitleOpts>
+                    <HomeTitleOpt>For you <Box position={"absolute"} left={"40%"} bottom={-3} borderRadius={"20px"} width={"60px"} height={"5px"} bgcolor={"#1d9bf0"} ></Box></HomeTitleOpt>
+                    <HomeTitleOpt>Following</HomeTitleOpt>
+                </HomeTitleOpts>
+            </HomeTitle>
+            <HomeContent>
+                <Avatar />
+                <Box component={"form"} flex={1}>
+                    <TweetField
+                        placeholder='What is happening?!'
+                        fullWidth
+                        value={tweet}
+                        onChange={(e) => setTweet(e.target.value)}
+                    />
+                    <TweetBtns>
+                        <Stack direction={"row"} gap={"5px"}>
+                            <Tooltip title="Media">
+                                <ImageOutlinedIcon />
+                            </Tooltip>
+                            <Tooltip title="GIF">
+                                <GifBoxOutlinedIcon />
+                            </Tooltip>
+                            <Tooltip title="Poll">
+                                <ChecklistRoundedIcon />
+                            </Tooltip>
+                            <Tooltip title="Emoji">
+                                <SentimentSatisfiedAltRoundedIcon />
+                            </Tooltip>
+                            <Tooltip title="Schedule">
+                                <EventAvailableOutlinedIcon />
+                            </Tooltip>
+                            <Tooltip title="Location">
+                                <PlaceOutlinedIcon />
+                            </Tooltip>
+                        </Stack>
+
+                        <Button type='submit' disabled={Boolean(!tweet)}>Tweet</Button>
+                    </TweetBtns>
+                </Box>
+            </HomeContent>
+
+            <Tweets />
+            <Tweets />
+            <Tweets />
+            <Tweets />
+            <Tweets />
+        </HomeBox>
     )
 }
 
