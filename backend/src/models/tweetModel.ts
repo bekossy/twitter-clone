a15d99f8542b063feb9515950b6994f82baeeb37
Kaosi Ezealigo <ezealigokosiso@gmail.com>
2023-05-30 14:28:48 +0100

    adds profileName and username to tweetSchema

--- backend/src/models/tweetModel.ts
+++ backend/src/models/tweetModel.ts
@@ -4,6 +4,8 @@ const Schema = mongoose.Schema
 const tweetSchema = new Schema({
     tweet: String,
     user_id: {type: String, required: true},
+    profileName: {type:String, required: true},
+    username: {type: String, required: true},
 }, {timestamps: true})
 
 const Tweet = mongoose.model("Tweet", tweetSchema)
