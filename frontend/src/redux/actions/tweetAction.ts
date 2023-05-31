77a4b73ac0985b9fb79f1aba4bfc5ee4c6cb253e
Kaosi Ezealigo <ezealigokosiso@gmail.com>
2023-05-31 13:21:30 +0100

    creates post action

--- frontend/src/redux/actions/tweetAction.ts
+++ frontend/src/redux/actions/tweetAction.ts
@@ -15,3 +15,12 @@ export const getTweets = ():any => async (dispatch:any, getState: () => RootStat
         dispatch({type: actionTypes.GET_TWEETS_FAIL, payload: error.response})
     }
 }
+
+export const postTweets = (tweet:string):any => async (dispatch:any, getState: () => RootState) => {
+    const {user} = getState().auth
+    const {data} = await axios.post("http://localhost:5000/api/tweets", {tweet}, {
+        headers: {"Authorization": `Bearer ${user.token}`}
+    });
+
+    dispatch({type: actionTypes.POST_TWEETS, payload: data})
+}
