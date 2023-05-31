6d9b3d65e215793c4b8dd2c940dc46ba15b0e0d5
Kaosi Ezealigo <ezealigokosiso@gmail.com>
2023-05-31 12:13:37 +0100

    constants for fetch actions

--- frontend/src/redux/constants/tweetConstants.ts
+++ frontend/src/redux/constants/tweetConstants.ts
@@ -2,3 +2,6 @@ export const GET_TWEETS = "GET_TWEETS";
 export const GET_TWEETS_SUCCESS = "GET_TWEETS_SUCCESS";
 export const GET_TWEETS_FAIL = "GET_TWEETS_FAIL";
 
+export const POST_TWEETS = "POST_TWEETS";
+export const DELETE_TWEET = "DELETE_TWEET";
+export const EDIT_TWEET = "EDIT_TWEET"
