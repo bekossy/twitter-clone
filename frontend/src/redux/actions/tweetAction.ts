4f7f0b724e3517c62478806457b1101692e90365
Kaosi Ezealigo <ezealigokosiso@gmail.com>
2023-06-01 14:48:10 +0100

    creates actions for post and delete tweets

--- frontend/src/redux/actions/tweetAction.ts
+++ frontend/src/redux/actions/tweetAction.ts
@@ -5,7 +5,7 @@ import { RootState } from "../store";
 export const getTweets = ():any => async (dispatch:any, getState: () => RootState) => {
     const {user} = getState().auth;
     try {
-        dispatch({type: actionTypes.GET_TWEETS});
+        dispatch({type: actionTypes.GET_TWEETS_REQUEST});
         const {data} = await axios.get("http://localhost:5000/api/tweets", {
             headers: {"Authorization": `Bearer ${user.token}`}
         });
@@ -18,9 +18,28 @@ export const getTweets = ():any => async (dispatch:any, getState: () => RootStat
 
 export const postTweets = (tweet:string):any => async (dispatch:any, getState: () => RootState) => {
     const {user} = getState().auth
-    const {data} = await axios.post("http://localhost:5000/api/tweets", {tweet}, {
-        headers: {"Authorization": `Bearer ${user.token}`}
-    });
+    try {
+        dispatch({type: actionTypes.POST_TWEET_REQUEST});
+        const {data} = await axios.post("http://localhost:5000/api/tweets", {tweet}, {
+            headers: {"Authorization": `Bearer ${user.token}`}
+        });
 
-    dispatch({type: actionTypes.POST_TWEETS, payload: data})
+        dispatch({type: actionTypes.POST_TWEET_SUCCESS, payload: data})
+    } catch (error:any) {
+        dispatch({type: actionTypes.POST_TWEET_FAIL, payload: error.response})
+    }
+}
+
+export const deleteTweet = (id:string):any =>async (dispatch:any, getState:()=>RootState) => {
+    const {user} = getState().auth;
+    try {
+        dispatch({type: actionTypes.DELETE_TWEET_REQUEST});
+        const {data} = await axios.delete(`http://localhost:5000/api/tweets/${id}`, {
+            headers: {"Authorization": `Bearer ${user.token}`}
+        });
+
+        dispatch({type: actionTypes.DELETE_TWEET_SUCCESS, payload: data})
+    } catch (error:any) {
+        dispatch({type: actionTypes.DELETE_TWEET_FAIL, payload: error.response})
+    }
 }
